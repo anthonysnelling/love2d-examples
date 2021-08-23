4136cf87776d0958f02cae94d82373c0d94163e0
Anthony Snelling <anthony.snelling@live.com>
2021-08-23 18:03:02 -0400

    added character mouse rotation, started work on zombie enemies, can spawn with space, they rotate to player.

--- TopDownShooter/main.lua
+++ TopDownShooter/main.lua
@@ -13,7 +13,9 @@ function love.load()
     player.y = love.graphics.getHeight()/2
     --properties
     player.speed = 180
-    
+
+
+    zombies = {}
 end
 
 function love.update(dt)
@@ -30,10 +32,37 @@ function love.update(dt)
         player.y = player.y + player.speed * dt
     end
 
+
 end
 
 function love.draw()
    love.graphics.draw(sprites.background, 0, 0) 
 
-   love.graphics.draw(sprites.player, player.x, player.y)
+   love.graphics.draw(sprites.player, player.x, player.y, PlayerMouseAngle(), nil, nil, sprites.player:getWidth()/2, sprites.player:getHeight()/2)
+
+   for index, zombo in ipairs(zombies) do
+      love.graphics.draw(sprites.zombie, zombo.x, zombo.y, ZombiePlayerAngle(zombo), nil, nil, sprites.zombie:getWidth()/2, sprites.zombie:getHeight()/2) 
+   end
 end
+
+function love.keypressed(key)
+   if key == "space" then
+      spawnZombie() 
+   end 
+end
+
+function PlayerMouseAngle()
+      return math.atan2(player.y - love.mouse.getY(), player.x - love.mouse.getX()) + math.pi
+end
+
+function spawnZombie()
+    local zombie = {}
+   zombie.x = math.random(0, love.graphics.getWidth())
+   zombie.y = math.random(0, love.graphics.getHeight()) 
+   zombie.speed = 100
+   table.insert(zombies, zombie)
+end
+
+function ZombiePlayerAngle(enemy)
+      return math.atan2(player.y - enemy.y, player.x - enemy.x ) 
+    end
