b241577f262ed0c7e5ff3b62349fc388c3281276
Anthony Snelling <anthony.snelling@live.com>
2021-09-07 17:42:01 -0400

    separated player functions in main.lua into player.lua for easy navigation and code cleanup

--- Platformer/main.lua
+++ Platformer/main.lua
@@ -21,13 +21,7 @@ function love.load()
   world:addCollisionClass('Player' --[[  the code in the brackets would ignore the platform,{ignores = {'Platform'}} ]] )
   world:addCollisionClass('Danger')
 
-  player = world:newRectangleCollider(360, 100, 40, 100, {collision_class = "Player"})
-  player:setFixedRotation(true)
-  player.speed = 240
-  player.animation = animations.idle
-  player.isMoving = false
-  player.direction = 1
-  player.grounded = true
+  require('player')
 
   platform = world:newRectangleCollider(259,400, 300,100, {collision_class = "Platform"})
   platform:setType('static')
@@ -38,53 +32,12 @@ end
 
 function love.update(dt)
    world:update(dt) 
-
-   -- check if the player is on the ground
-    if player.body  then
-      local colliders = world:queryRectangleArea(player:getX() - 20, player:getY() + 50, 40, 2, {'Platform'})
-      if #colliders > 0 then
-         player.grounded = true
-      else
-         player.grounded = false
-      end
-
-      player.isMoving = false
-       local px, py = player:getPosition()
-
-    if love.keyboard.isDown('d') or love.keyboard.isDown('right') then
-       player:setX(px + (player.speed * dt)) 
-       player.isMoving = true
-       player.direction = 1
-    end
-    if love.keyboard.isDown('a') or love.keyboard.isDown('left') then
-       player:setX(px - (player.speed * dt)) 
-       player.isMoving = true
-       player.direction = -1
-    end
-
-    if player:enter('Danger') then
-       player:destroy()
-    end
-   end
-
-if player.grounded then
-    if player.isMoving == true then
-       player.animation = animations.run      
-    elseif player.isMoving == false then 
-       player.animation = animations.idle
-    end 
-   else 
-      player.animation = animations.jump
-   end
-    player.animation:update(dt)
+   playerUpdate(dt)
 end
 
 function love.draw()
    world:draw() 
-
-   local px,py = player:getPosition()
-   -- multiply scale by player.direction to scale
-   player.animation:draw(sprites.playerSheet,px, py, nil, 0.25 * player.direction, 0.25, 130, 300)
+   drawPlayer()
 end
 
 function love.keypressed(key)
